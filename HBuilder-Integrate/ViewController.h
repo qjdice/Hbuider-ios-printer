5b1426d991c015cb9a034ff3f8ad5da4660e6b0c
qjdice <qjl516585657@163.com>
2018-09-25 14:04:26 +0800

    update viewcontroller

--- HBuilder-Integrate/ViewController.h
+++ HBuilder-Integrate/ViewController.h
@@ -7,15 +7,10 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 #import "PDRCore.h"
 #import "PDRCoreAppWindow.h"
 
 @interface ViewController : UIViewController<PDRCoreDelegate,PDRCoreAppWindowDelegate>
-{
-    IBOutlet UIButton *btnShowLocalPage;
-    IBOutlet UIButton *btnShowLocalPage2;
-}
 
--(IBAction)ShowWebViewPageOne:(id)sender;
--(IBAction)ShowWebViewPageTwo:(id)sender;
 @end
--- HBuilder-Integrate/ViewController.m
+++ HBuilder-Integrate/ViewController.m
@@ -6,80 +6,206 @@
 //  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
 //
 #import "ViewController.h"
-#import "WebViewController.h"
-#import "WebAppController.h"
 
+#import "PDRToolSystem.h"
+#import "PDRToolSystemEx.h"
+#import "PDRCoreAppManager.h"
+
+#define kStatusBarHeight 20.f
 @interface ViewController ()
 {
-    WebViewController* pWebViewController;
-    WebAppController* pWebAppController;
+    PDRCoreApp* pAppHandle;
+    BOOL _isFullScreen;
+    UIStatusBarStyle _statusBarStyle;
 }
 
 @end
-
+static UIView* pContentVIew = nil;
 
 @implementation ViewController
 
 
-#pragma mark 应用集成
 -(void)viewDidLoad {
     [super viewDidLoad];
-    pWebAppController = [[WebAppController alloc] init];
-    if (pWebAppController) {
-        self.navigationController.navigationBarHidden = YES;
-        [self.navigationController pushViewController:pWebAppController animated:YES];
-        //        [pWebAppController release];
-    }
-}
-
--(IBAction)ShowWebViewPageOne:(id)sender
-{
-    pWebViewController = [[WebViewController alloc] init];
-    if (pWebViewController)
-    {
-        [self.navigationController pushViewController:pWebViewController animated:YES];
-//        [pWebViewController release];
-    }
+//    pWebAppController = [[WebAppController alloc] init];
+//    if (pWebAppController) {
+//        self.navigationController.navigationBarHidden = YES;
+//        [self.navigationController pushViewController:pWebAppController animated:YES];
+//        //        [pWebAppController release];
+//    }
     
-    // 添加一个原生层的消息监听，可以监听页面中通过NJS发送的消息，并获取附带的数据
-    // NJS 发送消息请参考Pandora/apps/HelloH5/plugin.html 文件的PostNotification方法
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(NotiFunction:) name:@"SendDataToNative" object:nil];
+    if(pContentVIew == nil)
+        pContentVIew = [[UIView alloc] initWithFrame:self.view.bounds];
+    [self.view addSubview: pContentVIew];
+    
+    PDRCore *h5Engine = [PDRCore Instance];
+    [self setStatusBarStyle:h5Engine.settings.statusBarStyle];
+    _isFullScreen = [UIApplication sharedApplication].statusBarHidden;
+    if ( _isFullScreen != h5Engine.settings.fullScreen ) {
+        _isFullScreen = h5Engine.settings.fullScreen;
+        if ( [self  respondsToSelector:@selector(setNeedsStatusBarAppearanceUpdate)] ) {
+            [self setNeedsStatusBarAppearanceUpdate];
+        } else {
+            [[UIApplication sharedApplication] setStatusBarHidden:_isFullScreen];
+        }
+    }
+    h5Engine.coreDeleagete = self;
+    h5Engine.persentViewController = self;
+    
+    // 设置WebApp所在的目录，该目录下必须有mainfest.json
+    NSString* pWWWPath = [[[NSBundle mainBundle] bundlePath] stringByAppendingPathComponent:@"Pandora/apps/HelloH5/www"];
+    
+    // 如果路径中包含中文，或Xcode工程的targets名为中文则需要对路径进行编码
+    //NSString* pWWWPath2 =  (NSString *)CFURLCreateStringByAddingPercentEscapes( kCFAllocatorDefault, (CFStringRef)pTempString, NULL, NULL,  kCFStringEncodingUTF8 );
+    
+    // 用户在集成5+SDK时，需要在5+内核初始化时设置当前的集成方式，
+    // 请参考AppDelegate.m文件的- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions方法
+    
+    // 设置5+SDK运行的View
+    [[PDRCore Instance] setContainerView:pContentVIew];
+    
+    // 传入参数可以在页面中通过plus.runtime.arguments参数获取
+    NSString* pArgus = @"id=plus.runtime.arguments";
+    // 启动该应用
+    pAppHandle = [[[PDRCore Instance] appManager] openAppAtLocation:pWWWPath withIndexPath:@"index.html" withArgs:pArgus withDelegate:nil];
+    
+    
+    // 如果应用可能会重复打开的话建议使用restart方法
+    //[[[PDRCore Instance] appManager] restart:pAppHandle];
+    
 }
 
 
-- (void)NotiFunction:(NSNotification*)pNoti
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+}
+
+- (void)viewDidUnload
 {
-    if (pNoti) {
-        NSString* pRecData = pNoti.object;
-        if (pRecData) {
-            NSLog(@"Native Receive Data:%@", pRecData);
-            UIAlertView* pAlertView = [[UIAlertView alloc] initWithTitle:@"原生层收到消息" message:pRecData delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
-            if (pAlertView) {
-                [pAlertView show];
-//                [pAlertView release];
-            }
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+#pragma mark -
+- (void)willAnimateRotationToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation
+                                         duration:(NSTimeInterval)duration
+{
+    [[PDRCore Instance] handleSysEvent:PDRCoreSysEventInterfaceOrientation
+                            withObject:[NSNumber numberWithInt:toInterfaceOrientation]];
+    if ([PTDeviceOSInfo systemVersion] >= PTSystemVersion8Series) {
+        [[UIApplication sharedApplication] setStatusBarHidden:_isFullScreen ];
+    }
+}
+
+- (BOOL)shouldAutorotate
+{
+    return TRUE;
+}
+
+- (UIInterfaceOrientationMask)supportedInterfaceOrientations
+{
+    return [[PDRCore Instance].settings supportedInterfaceOrientations];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    if ( [PDRCore Instance].settings ) {
+        return [[PDRCore Instance].settings supportsOrientation:interfaceOrientation];
+    }
+    return UIInterfaceOrientationPortrait == interfaceOrientation;
+}
+
+- (BOOL)prefersStatusBarHidden
+{
+    return _isFullScreen;/*
+                          NSString *model = [UIDevice currentDevice].model;
+                          if (UIUserInterfaceIdiomPhone == UI_USER_INTERFACE_IDIOM()
+                          && (NSOrderedSame == [@"iPad" caseInsensitiveCompare:model]
+                          || NSOrderedSame == [@"iPad Simulator" caseInsensitiveCompare:model])) {
+                          return YES;
+                          }
+                          return NO;*/
+}
+
+- (UIStatusBarAnimation)preferredStatusBarUpdateAnimation {
+    return UIStatusBarAnimationFade;
+}
+
+-(BOOL)getStatusBarHidden {
+    if ( [PTDeviceOSInfo systemVersion] > PTSystemVersion6Series ) {
+        return _isFullScreen;
+    }
+    return [UIApplication sharedApplication].statusBarHidden;
+}
+
+#pragma mark - StatusBarStyle
+-(UIStatusBarStyle)getStatusBarStyle {
+    return [self preferredStatusBarStyle];
+}
+-(void)setStatusBarStyle:(UIStatusBarStyle)statusBarStyle {
+    if ( _statusBarStyle != statusBarStyle ) {
+        _statusBarStyle = statusBarStyle;
+        if ( [self  respondsToSelector:@selector(setNeedsStatusBarAppearanceUpdate)] ) {
+            [self setNeedsStatusBarAppearanceUpdate];
+        } else {
+            [[UIApplication sharedApplication] setStatusBarStyle:_statusBarStyle];
         }
     }
 }
 
-
-
--(IBAction)ShowWebViewPageTwo:(id)sender
+- (UIStatusBarStyle)preferredStatusBarStyle
 {
-    // Webivew集成不能同时WebApp集成，需要修改AppDelegate文件的PDRCore的启动参数
-    pWebAppController = [[WebAppController alloc] init];
-    if (pWebAppController) {
-        self.navigationController.navigationBarHidden = YES;
-        [self.navigationController pushViewController:pWebAppController animated:YES];
-//        [pWebAppController release];
-    }
+    return _statusBarStyle;
 }
 
-
+#pragma mark -
+-(void)wantsFullScreen:(BOOL)fullScreen
+{
+    if ( _isFullScreen == fullScreen ) {
+        return;
+    }
+    
+    _isFullScreen = fullScreen;
+    [[UIApplication sharedApplication] setStatusBarHidden:_isFullScreen withAnimation:_isFullScreen?NO:YES];
+    if ( [PTDeviceOSInfo systemVersion] > PTSystemVersion6Series ) {
+        [self setNeedsStatusBarAppearanceUpdate];
+    }// else {
+    //   return;
+    //  }
+    CGRect newRect = self.view.bounds;
+    if ( [PTDeviceOSInfo systemVersion] <= PTSystemVersion6Series ) {
+        newRect = [UIApplication sharedApplication].keyWindow.bounds;
+        if ( _isFullScreen ) {
+            [UIView beginAnimations:nil context:nil];
+            self.view.frame = newRect;
+            [UIView commitAnimations];
+        } else {
+            UIInterfaceOrientation interfaceOrientation = [UIApplication sharedApplication].statusBarOrientation;
+            if ( UIDeviceOrientationLandscapeLeft == interfaceOrientation
+                || interfaceOrientation == UIDeviceOrientationLandscapeRight ) {
+                newRect.size.width -=kStatusBarHeight;
+            } else {
+                newRect.origin.y += kStatusBarHeight;
+                newRect.size.height -=kStatusBarHeight;
+            }
+            [UIView beginAnimations:nil context:nil];
+            self.view.frame = newRect;
+            [UIView commitAnimations];
+        }
+        [[PDRCore Instance] handleSysEvent:PDRCoreSysEventInterfaceOrientation
+                                withObject:[NSNumber numberWithInt:0]];
+        
+    }
+}
 
 - (void)didReceiveMemoryWarning{
     [[PDRCore Instance] handleSysEvent:PDRCoreSysEventReceiveMemoryWarning withObject:nil];
 }
+
+- (void)dealloc {
+    //    [super dealloc];
+}
+
 
 //- (void)dealloc {
 //    [super dealloc];
